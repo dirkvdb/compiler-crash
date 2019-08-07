e6f40241b7c54f930b019c00bbc34142ba0748b9
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2019-08-07 14:38:27 +0200

    Sqlpp updates

--- build.bat
+++ build.bat
@@ -1,14 +1,13 @@
 SET projectdir=%cd%
-CALL "C:\Program Files (x86)\Microsoft Visual Studio\Preview\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
 cd %projectdir%
 
 mkdir build
-mkdir build\
 cd build\
 
 cmake ^
-    -G "Visual Studio 15 2017" ^
-    -DCMAKE_GENERATOR_PLATFORM=x64 ^
+    -G "Visual Studio 16 2019" ^
+    -A x64 ^
     ..
 cmake --build . --config Release
 pause
